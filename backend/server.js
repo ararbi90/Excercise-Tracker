21b9934aa2fce34e56da340ed0a9aaf48edfb5bc
ararbi90 <ararbi90@hotmail.com>
2021-01-17 16:30:16 -0800

    Added static render

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,14 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const path = require("path");
 
 require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 5000;
-app.use(express.static('../build'))
+app.use(express.static(path.join(__dirname, "..", "build")));
+app.use(express.static("public"));
 app.use(cors());
 app.use(bodyParser.json());
 
